056f4f1471982890a980a359de405832699251a9
ABDERRAHIM AIT TATA <39503536+aittata@users.noreply.github.com>
2021-04-17 17:33:38 +0000

    Commit

---
+++ lib/widgets/calls_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CallsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      // child: Expanded(
+      //   child: ListView.builder(
+      //     padding: EdgeInsets.all(5),
+      //     physics: BouncingScrollPhysics(),
+      //     itemCount: contactsList.length,
+      //     itemBuilder: (context, index) {
+      //       Contact contact = contactsList[index];
+      //       return ContactShape(contact: contact);
+      //     },
+      //   ),
+      // ),
+    );
+  }
+}
