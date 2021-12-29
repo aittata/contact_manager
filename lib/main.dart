9b55be1414588f7eb4e07e41f8dfc4e871f3f6ea
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-29 17:16:22 +0100

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
-import 'package:contact_manager/constant/constant.dart';
-import 'package:contact_manager/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:get/get.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData.dark().copyWith(
-        primaryColor: mainColor,
-        scaffoldBackgroundColor: backColor,
-      ),
-      home: InitialScreen(),
-    );
-  }
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
