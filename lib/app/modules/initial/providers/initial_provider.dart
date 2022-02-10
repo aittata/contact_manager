58b96491a86807be140666bf2c0a1cd361fef7d2
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-02-10 20:01:40 +0100

    Commit

--- lib/app/modules/initial/providers/initial_provider.dart
+++ lib/app/modules/initial/providers/initial_provider.dart
@@ -4,19 +4,5 @@ class InitialProvider extends GetConnect {
   @override
   void onInit() {}
 
-  get getContacts async {
-    // var data = await ContactsService.getContacts();
-    // print(data);
-    // bool isContactsGranted = await Permission.contacts.request().isGranted;
-    try {
-      //if (isContactsGranted) {
-      //List<Contact> _contactsList = (await ContactsService.getContacts()).toList();
-      //print(_contactsList);
-      //contactsList = _contactsList;
-
-      //}
-    } catch (e) {
-      throw Exception("Something went Wrong");
-    }
-  }
+  get getContacts async {}
 }
