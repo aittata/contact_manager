59f1d148273b2bb33e1a1ddb7b60812fd82aaf8a
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-02-11 22:23:04 +0100

    Commit

--- lib/app/modules/initial/providers/initial_provider.dart
+++ lib/app/modules/initial/providers/initial_provider.dart
@@ -1,22 +1,24 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class InitialProvider extends GetConnect {
   @override
   void onInit() {}
 
   get _getPermission async {
-    // PermissionStatus permissionStatus = await Permission.contacts.status;
-    // if (!permissionStatus.isGranted && !permissionStatus.isPermanentlyDenied) {
-    //   permissionStatus = await Permission.contacts.request();
-    // }
-    // return permissionStatus;
+    PermissionStatus permissionStatus = await Permission.contacts.status;
+    if (!permissionStatus.isGranted && !permissionStatus.isPermanentlyDenied) {
+      permissionStatus = await Permission.contacts.request();
+    }
+    return permissionStatus;
   }
 
   get getContacts async {
-    // final PermissionStatus permissionStatus = await _getPermission;
-    // if (permissionStatus.isGranted) {
-    //   final Iterable<Contact> contacts = await ContactsService.getContacts();
-    //   return contacts;
-    // }
+    final PermissionStatus permissionStatus = await _getPermission;
+    if (permissionStatus.isGranted) {
+      final Iterable<Contact> contacts = await ContactsService.getContacts();
+      return contacts;
+    }
   }
 }
