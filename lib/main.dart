1d96cc5706d7ae0875bd261d15feea1cc7da0f3d
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-02-15 22:11:58 +0100

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class ContactManager extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: AppMessage.appTitle,
       theme: AppTheme.themeData,
-      getPages: AppPages.routes,
-      // initialRoute: AppPages.INITIAL,
+      // getPages: AppPages.routes,
+      initialRoute: AppPages.INITIAL,
     );
   }
 }
